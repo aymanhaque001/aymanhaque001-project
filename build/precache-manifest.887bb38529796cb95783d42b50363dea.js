ee82d035c582f9676a1f2abc05b4c33fe5a98008
AYMAN HAQUE <aymanhaque001@gmail.com>
2020-04-10 23:22:08 -0500

    using youtube data API
    integrating youtube video channels to our app

---
+++ build/precache-manifest.887bb38529796cb95783d42b50363dea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41aebab45d1b7e92aa5b7b8a2af15481",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffa9b1ba4ae996bf5867",
+    "url": "/static/css/main.81fdcfb1.chunk.css"
+  },
+  {
+    "revision": "1cc6ca57f2d6d5e8685c",
+    "url": "/static/js/2.be13dbee.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.be13dbee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffa9b1ba4ae996bf5867",
+    "url": "/static/js/main.0e345519.chunk.js"
+  },
+  {
+    "revision": "8dee6076c494dc7d176e",
+    "url": "/static/js/runtime-main.6d9b94cf.js"
+  }
+]);
